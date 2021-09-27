9a96f8237550ec11e3b383efa8f2075d04374230
Chinmay Karmokar <chinmaykarmokar@gmail.com>
2021-09-27 18:18:34 +0530

    Small change.

--- pages/components/molecules/Latest.js
+++ pages/components/molecules/Latest.js
@@ -1,14 +1,13 @@
-import { Flex, Button, Heading, Text, Image } from "@chakra-ui/react";
+import { Flex, Button, Heading, Text, Image, Divider } from "@chakra-ui/react";
 
 const LatestNews = () => {
     return (
         <>
             <Flex border="1px solid #c6c6c6" m="3" bgGradient="linear(to-b, #f0f5f5, #fff, #f0f5f5)" borderRadius="10px" height="250px" width="300px" flexWrap="wrap" p={5}>
-               <Heading fontSize="25px" color="#354882">Benefit Calculator</Heading>
-               <br/><br/> 
-               <Text>Find out what you can afford using a simple housing benefit calculator.</Text>
-               <Image src="http://www.dssmove.co.uk/default/public/media/images/graphic-housing.jpg" alt="Description"/>
-               <Button backgroundColor="#354882" color="#fff" mt="5px" width="100%">Calculate Benefit</Button>
+               <Heading fontSize="25px" color="#354882">Latest news</Heading>
+               <Divider/>
+               <Text>Benefit claimants' personal data to be shared from February - 27/01/15</Text>
+               <Text>50% of homeless made homeless under 21 - 07/01/15</Text>
             </Flex>
         </>
     )
