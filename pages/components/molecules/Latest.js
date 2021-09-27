52e04deffe8f951ae23e3434452b6d40802eb860
Chinmay Karmokar <chinmaykarmokar@gmail.com>
2021-09-27 18:25:33 +0530

    Small change.

--- pages/components/molecules/Latest.js
+++ pages/components/molecules/Latest.js
@@ -1,13 +1,16 @@
-import { Flex, Button, Heading, Text, Image, Divider } from "@chakra-ui/react";
+import { Flex, Heading, Text, Divider } from "@chakra-ui/react";
 
 const LatestNews = () => {
     return (
         <>
-            <Flex border="1px solid #c6c6c6" m="3" bgGradient="linear(to-b, #f0f5f5, #fff, #f0f5f5)" borderRadius="10px" height="250px" width="300px" flexWrap="wrap" p={5}>
-               <Heading fontSize="25px" color="#354882">Latest news</Heading>
-               <Divider/>
-               <Text>Benefit claimants' personal data to be shared from February - 27/01/15</Text>
-               <Text>50% of homeless made homeless under 21 - 07/01/15</Text>
+            <Flex border="1px solid #c6c6c6" borderRadius="10px" m="3" width="300px" flexWrap="wrap">
+                <Flex width="100%" bgGradient="linear(to-b, #f0f5f5, #fff, #f0f5f5)" height="50px" color="#354882" borderBottom="1px solid #c6c6c6" borderTopLeftRadius="10px" borderTopRightRadius="10px">
+                    <Heading ml={5} mt={2.5} fontSize="25px">Latest News</Heading>
+                </Flex>
+                <Flex flexWrap="wrap" p={5}>
+                    <Text><b>Benefit claimants personal data to be shared from February - 27.01.15</b></Text>
+                    <Text><b>50% of homeless made homeless under 21 - 07.01.15</b></Text>
+                </Flex>
             </Flex>
         </>
     )
